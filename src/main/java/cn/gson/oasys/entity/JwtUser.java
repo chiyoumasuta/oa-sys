2b66fd56bc3c05018c37d039a677add3076cdee5
chiyoumasuta <zb25801@outlook.com>
2024-09-10 23:26:12 +0800

    cli

---
+++ src/main/java/cn/gson/oasys/entity/JwtUser.java
@@ -0,0 +1,20 @@
+package cn.gson.oasys.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @author l
+ */
+@Data
+@Accessors(chain = true)
+public class JwtUser {
+
+    private boolean valid;
+    private String userId;
+    private String role;
+
+    public JwtUser() {
+        this.valid = false;
+    }
+}

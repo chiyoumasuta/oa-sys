58548a2f5d55433515174a19e8dced0f18e673fe
chiyoumasuta <zb25801.@outlook.com>
2024-09-14 16:06:35 +0800

    update

--- src/main/java/cn/gson/oasys/flowable/AppDispatcherServletConfiguration.java
+++ src/main/java/cn/gson/oasys/flowable/AppDispatcherServletConfiguration.java
@@ -1,52 +1,52 @@
-//package cn.gson.oasys.flowable;
-//
-//import org.flowable.ui.modeler.rest.app.EditorGroupsResource;
-//import org.flowable.ui.modeler.rest.app.EditorUsersResource;
-//import org.flowable.ui.modeler.rest.app.StencilSetResource;
-//import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.FilterType;
-//import org.springframework.scheduling.annotation.EnableAsync;
-//import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-//import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-//import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-//
-//@Configuration
-//@ComponentScan(value = { "org.flowable.ui.modeler.rest.app",
-//        // 不加载 rest，因为 getAccount 接口需要我们自己实现
-////        "org.flowable.ui.common.rest"
-//    },excludeFilters = {
-//        // 移除 EditorUsersResource 与 EditorGroupsResource，因为不使用 IDM 部分
-//        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = EditorUsersResource.class),
-//        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = EditorGroupsResource.class),
-//        // 配置文件用自己的
-//        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = StencilSetResource.class),
-//    }
-//)
-//@EnableAsync
-//public class AppDispatcherServletConfiguration implements WebMvcRegistrations {
-//
-//    @Bean
-//    public SessionLocaleResolver localeResolver() {
-//        return new SessionLocaleResolver();
-//    }
-//
-//    @Bean
-//    public LocaleChangeInterceptor localeChangeInterceptor() {
-//        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-//        localeChangeInterceptor.setParamName("language");
-//        return localeChangeInterceptor;
-//    }
-//
-//    @Override
-//    public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
-//        RequestMappingHandlerMapping requestMappingHandlerMapping = new RequestMappingHandlerMapping();
-//        requestMappingHandlerMapping.setUseSuffixPatternMatch(false);
-//        requestMappingHandlerMapping.setRemoveSemicolonContent(false);
-//        Object[] interceptors = { localeChangeInterceptor() };
-//        requestMappingHandlerMapping.setInterceptors(interceptors);
-//        return requestMappingHandlerMapping;
-//    }
-//}
+package cn.gson.oasys.flowable;
+
+import org.flowable.ui.modeler.rest.app.EditorGroupsResource;
+import org.flowable.ui.modeler.rest.app.EditorUsersResource;
+import org.flowable.ui.modeler.rest.app.StencilSetResource;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@Configuration
+@ComponentScan(value = { "org.flowable.ui.modeler.rest.app",
+        // 不加载 rest，因为 getAccount 接口需要我们自己实现
+//        "org.flowable.ui.common.rest"
+    },excludeFilters = {
+        // 移除 EditorUsersResource 与 EditorGroupsResource，因为不使用 IDM 部分
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = EditorUsersResource.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = EditorGroupsResource.class),
+        // 配置文件用自己的
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = StencilSetResource.class),
+    }
+)
+@EnableAsync
+public class AppDispatcherServletConfiguration implements WebMvcRegistrations {
+
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("language");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+        RequestMappingHandlerMapping requestMappingHandlerMapping = new RequestMappingHandlerMapping();
+        requestMappingHandlerMapping.setUseSuffixPatternMatch(false);
+        requestMappingHandlerMapping.setRemoveSemicolonContent(false);
+        Object[] interceptors = { localeChangeInterceptor() };
+        requestMappingHandlerMapping.setInterceptors(interceptors);
+        return requestMappingHandlerMapping;
+    }
+}
